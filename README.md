09f199a8c226be2971aa41321b0563cab7c084cf
yilisharcs <yilisharcs@gmail.com>
2025-05-04 10:04:33 -0300

    take tj's markdown plugin video as inspiration

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # wikipedia.nvim
 
 Browse wikipedia articles from the comfort of your favorite editor.
+
+# Usage
+
+```lua
+require('wikibrowse')
+```
+
+# TODO
+
+- [ ] use command below to name the window after the title of the article
+
+```lua
+vim.api.nvim_create_autocmd("BufNew", {
+  pattern = "*",
+  callback = function()
+    if vim.bo.buftype == "nofile" then
+      vim.api.nvim_buf_set_name(0, "Scratch")
+    end
+  end,
+})```
--- lua/wikibrowse.lua
+++ lua/wikibrowse.lua
@@ -1,23 +1,43 @@
 local M = {}
 
 M.setup = function()
-
+  -- vim.api.nvim_create_user_command(
+  --   'WikiBrowse',
+  --   'lua WikiBrowse()',
+  --   {}
+  -- )
 end
 
-vim.api.nvim_create_user_command(
-  'WikiBrowse',
-  'lua WikiBrowse()',
-  {}
-)
+local function create_floating_window(opts)
+  opts = opts or {}
+  local width = opts.width or math.floor(vim.o.columns * 0.8)
+  local height = opts.height or math.floor(vim.o.lines * 0.8)
+  local col = math.floor((vim.o.columns - width) / 2)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local buf = vim.api.nvim_create_buf(false, true)
 
-function WikiBrowse()
-  local bufnr = 0
+  local win_config = {
+    relative = 'editor',
+    width = width,
+    height = height,
+    col = col,
+    row = row,
+    style = 'minimal',
+    border = 'rounded',
+  }
+  local win = vim.api.nvim_open_win(buf, true, win_config)
+
+  return { buf = buf, win = win }
+end
+
+M.wiki_search = function()
+  local float = create_floating_window()
 
   local on_exit = function(obj)
     vim.schedule(function()
       if obj.code == 0 and obj.stdout then
         local lines = vim.split(obj.stdout, '\n', { trimempty = true })
-        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+        vim.api.nvim_buf_set_lines(float.buf, 0, -1, false, lines)
       else
         local error_msg = obj.stderr or ("Exited with code: " .. obj.code)
         vim.notify(error_msg, vim.log.levels.ERROR)
@@ -28,8 +48,24 @@ function WikiBrowse()
   vim.system({
     'nu',
     '-c',
-    'http get https://en.wikipedia.org/wiki/Printing_press',
+    'ls **/*',
+    -- 'http get https://en.wikipedia.org/wiki/Printing_press',
   }, { text = true }, on_exit)
+
+  -- local current_slide = 1
+  -- vim.keymap.set('n', 'n', function()
+  --   current_slide = math.min(current_slide + 1, #parsed.slides)
+  --   vim.api.nvim_buf_set_lines(float.buf, 0, -1, false, parsed.slides[current_slide])
+  -- end, { buffer = float.buf })
+  -- vim.keymap.set('n', 'p', function()
+  --   current_slide = math.max(current_slide - 1, 1)
+  --   vim.api.nvim_buf_set_lines(float.buf, 0, -1, false, parsed.slides[current_slide])
+  -- end, { buffer = float.buf })
+  vim.keymap.set('n', 'q', function()
+    vim.api.nvim_win_close(float.win, true)
+  end, { buffer = float.buf })
 end
+
+-- M.WikiSearch()
 
 return M
