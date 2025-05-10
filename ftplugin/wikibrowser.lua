18b1508d680ebdf843b6e11d7fa6696a1851fbe0
yilisharcs <yilisharcs@gmail.com>
2025-05-10 09:34:48 -0300

    be more explicit with jump param

--- ftplugin/wikibrowser.lua
+++ ftplugin/wikibrowser.lua
@@ -13,6 +13,6 @@ vim.wo.conceallevel = 3
 local move = require('wikibrowse.index')
 local opts = { buffer = true }
 
-vim.keymap.set('n', 'j', function() move.cursor_jump('j') end, opts)
-vim.keymap.set('n', 'k', function() move.cursor_jump('k') end, opts)
+vim.keymap.set('n', 'j', function() move.cursor_jump('next') end, opts)
+vim.keymap.set('n', 'k', function() move.cursor_jump('prev') end, opts)
 vim.keymap.set('n', 'q', function() vim.api.nvim_win_close(0, true) end, opts)
--- lua/wikibrowse/index.lua
+++ lua/wikibrowse/index.lua
@@ -21,19 +21,19 @@ local get_article_index = function()
   return { prev = articles_prev, next = articles_next }
 end
 
-M.cursor_jump = function(key)
+M.cursor_jump = function(cmd)
   local cursor = vim.api.nvim_win_get_cursor(0)[1] -- get line, ignore row
   local index = get_article_index()
   local prev, next = index.prev, index.next
 
-  if key == 'k' then
+  if cmd == 'prev' then
     for _, i in ipairs(prev) do
       if i < cursor then
         vim.api.nvim_win_set_cursor(0, { i, 0 })
         break
       end
     end
-  elseif key == 'j' then
+  elseif cmd == 'next' then
     for _, i in ipairs(next) do
       if i > cursor then
         vim.api.nvim_win_set_cursor(0, { i, 0 })
